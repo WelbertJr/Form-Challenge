649b70bad82d116794e7f086564d91660c95d985
WelbertJr <welbertjunior@live.com>
2022-06-05 23:50:55 -0300

    initiating responsiveness #3

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,12 @@ const containerSucces = document.querySelector('.section-page2');
 
 btn.addEventListener('click', function() {
     container.style.display = 'none';
-    containerSucces.style.display = 'flex';
+    containerSucces.style.display = 'block';
 
 })
 
 const btn2 = document.querySelector('#input-button2');
 btn2.addEventListener('click', function() {
     containerSucces.style.display = 'none';
-    container.style.display = 'flex';
+    container.style.display = 'block';
 })
